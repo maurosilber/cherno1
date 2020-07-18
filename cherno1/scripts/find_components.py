2125d050918f2174c84308f14fd19ad14b99781d
Mauro Silberberg <maurosilber@gmail.com>
2020-07-18 18:26:38 -0300

    Added find_components script

---
+++ cherno1/scripts/find_components.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import click
+import numpy as np
+from cherno1.components import find_components
+
+
+@click.command()
+@click.argument("file", type=click.Path(exists=True, dir_okay=False))
+@click.argument("threshold", type=click.INT)
+@click.option(
+    "--outdir",
+    default=".",
+    type=click.Path(exists=True, file_okay=False),
+    help="Directory to save the output.",
+)
+@click.option(
+    "--n", default=-1, type=click.INT, help="Only process the first N repeats."
+)
+def components(file, threshold, outdir, n):
+    """Calculate components at similarity >= THRESHOLD from repeats file FILE."""
+    outfile = Path(outdir) / f"components_{threshold:02}.npy"
+    data = np.load(file)[:n]
+    components = find_components(data, threshold)
+    np.save(outfile, components)
+
+
+if __name__ == "__main__":
+    components()
