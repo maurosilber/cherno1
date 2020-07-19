47b2ad826e4e529036900a41d5221256b08ec05d
Mauro Silberberg <maurosilber@gmail.com>
2020-07-19 18:44:17 -0300

    Changed Set to List in find_components
    There was a bug with numba. Non-numba compiled function run fine.
    Numba-compiled function failed in 1 particular case:
    
    array_map = np.load("../data/repeats_map.npy")
    repeats = np.load("../data/internal_repeats_num_ali.npy")
    order = np.argsort(array_map)
    x = array_map[order]
    i = 168222
    start, end = find_first_equal(x, i), find_first_equal(x, i + 1)
    x, order = x[start:], order[start:]
    array_repeats = repeats[order[:end]]
    components = np.zeros(len(array_repeats), int)
    find_component(array_repeats, components, 1, 11)

--- cherno1/components.py
+++ cherno1/components.py
@@ -1,21 +1,22 @@
 import numpy as np
 from numba import njit
+from numba.typed import List
 
 from .common import find_first_equal, find_last_equal, similarity
 
 
-@njit(inline="always")
-def find_component(data, components, label, threshold):
-    check = {0}
+@njit
+def find_component(repeats, components, label, threshold):
+    check = List([0])
     components[0] = label
     while check:
-        x = data[check.pop()]
+        x = repeats[check.pop()]
         for j in range(1, len(components)):
             if components[j] > 0:
                 continue
-            elif similarity(x, data[j]) >= threshold:
+            elif similarity(x, repeats[j]) >= threshold:
                 components[j] = label
-                check.add(j)
+                check.append(j)
 
 
 @njit
@@ -33,3 +34,34 @@ def find_components(data, threshold):
     components = np.zeros(n, np.min_scalar_type(n))
     max_label = _find_components(data, components, threshold)
     return components.astype(np.min_scalar_type(max_label))
+
+
+def min_similarity_for_all_connected_array(repeats, array_map):
+    n = array_map.max() + 1
+    k = repeats.shape[1] + 1
+    min_similarity = -np.ones((n, k), dtype=np.int8)
+
+    order = np.argsort(array_map)
+    x = array_map[order]
+
+    import tqdm
+
+    end = 0
+    for i in tqdm.trange(n):
+        x, order = x[end:], order[end:]
+        end = find_first_equal(x, i + 1)
+        array_repeats = repeats[order[:end]]
+        array_n = len(array_repeats)
+
+        if array_n == 1:
+            continue
+        elif array_n == 2:
+            s = similarity(*array_repeats)
+            min_similarity[i, :s] = 1
+        else:
+            for s in reversed(range(k)):
+                components = np.zeros(array_n, int)
+                max_label = _find_components(array_repeats, components, s)
+                min_similarity[i, s] = max_label
+
+    return min_similarity
--- cherno1/degree.py
+++ cherno1/degree.py
@@ -1,28 +1,28 @@
 import numpy as np
 from numba import njit, prange
 
-from .common import similarity
+from .common import find_first_equal, similarity
 
 
 @njit(inline="always")
-def _degree_for_x(data, x, degree_x):
-    for y in data:
-        d = similarity(x, y)
-        degree_x[d] += 1
+def _degree_by_similarity_for_repeat(repeats, repeat, degree_repeat):
+    for y in repeats:
+        d = similarity(repeat, y)
+        degree_repeat[d] += 1
 
     # Remove link with itself
-    d = similarity(x, x)
-    degree_x[d] -= 1
+    d = similarity(repeat, repeat)
+    degree_repeat[d] -= 1
 
 
 @njit(parallel=True)
-def _batch_update(data, data_batch, degree_batch):
-    for i in prange(len(data_batch)):
-        _degree_for_x(data, data_batch[i], degree_batch[i])
+def _batch_update(repeats, repeats_batch, degrees_batch):
+    for i in prange(len(repeats_batch)):
+        _degree_by_similarity_for_repeat(repeats, repeats_batch[i], degrees_batch[i])
 
 
-def degree_by_similarity(data, progress=False, batch_size=16):
-    n, k = data.shape
+def degree_by_similarity(repeats, progress=False, batch_size=16):
+    n, k = repeats.shape
     degree = np.zeros((n, k + 1), dtype=np.min_scalar_type(n))
 
     if progress:
@@ -30,8 +30,35 @@ def degree_by_similarity(data, progress=False, batch_size=16):
 
         for i in tqdm.trange(n):
             if i % batch_size == 0:
-                _batch_update(data, data[i:][:batch_size], degree[i:][:batch_size])
+                _batch_update(
+                    repeats, repeats[i:][:batch_size], degree[i:][:batch_size]
+                )
     else:
-        _batch_update(data, data, degree)
+        _batch_update(repeats, repeats, degree)
+
+    return degree.astype(np.min_scalar_type(degree.max()))
+
+
+@njit
+def _degree_by_similarity_for_array(array, degree):
+    for repeat in array:
+        _degree_by_similarity_for_repeat(array, repeat, degree)
+    degree //= 2
+
+
+def degree_by_similarity_by_array(repeats, array_map):
+    n = array_map.max() + 1
+    _, k = repeats.shape
+    degree = np.zeros((n, k + 1), dtype=int)
+
+    order = np.argsort(array_map)
+    x = array_map[order]
+
+    for i in range(n):
+        end = find_first_equal(x, i + 1)
+        if end > 1:
+            array = repeats[order[:end]]
+            _degree_by_similarity_for_array(array, degree[i])
+        x, order = x[end:], order[end:]
 
     return degree.astype(np.min_scalar_type(degree.max()))
