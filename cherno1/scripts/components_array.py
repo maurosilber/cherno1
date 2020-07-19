21d0714aa7061282958d1895c6c18a4c1d46344a
Mauro Silberberg <maurosilber@gmail.com>
2020-07-19 18:44:17 -0300

    Changed Set to List in find_components
    Added find_components_by_array
    
    There was a bug with numba. Non-numba compiled function run fine.
    Numba-compiled function failed in 1 particular case:
    
    array_map = np.load("../data/repeats_map.npy")
    repeats = np.load("../data/internal_repeats_num_ali.npy")
    order = np.argsort(array_map)
    x = array_map[order]
    i = 168222
    start, end = find_first_equal(x, i), find_first_equal(x, i + 1)
    x, order = x[start:], order[start:]
    array_repeats = repeats[order[:end]]
    components = np.zeros(len(array_repeats), int)
    find_component(array_repeats, components, 1, 11)

---
+++ cherno1/scripts/components_array.py
@@ -0,0 +1,20 @@
+"""
+Calculate in-array degree distribution for a given similarity threshold.
+
+OUTPUT: degree array of (N array, k similarity level)
+
+So, array[i,j] is the number of internal edges for array i at similarity j.
+"""
+
+import numpy as np
+
+from cherno1.components import find_components_by_array
+
+
+def _func():
+    array_map = np.load("../data/repeats_map.npy")
+    repeats = np.load("../data/internal_repeats_num_ali.npy")
+    return find_components_by_array(repeats, array_map)
+
+
+components_by_array = _func()
