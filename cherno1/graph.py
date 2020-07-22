eb6c770b14988ce9524f9a40f08a34476335be94
Mauro Silberberg <maurosilber@gmail.com>
2020-07-22 12:53:44 -0300

    Build graph

--- cherno1/graph.py
+++ cherno1/graph.py
@@ -1,4 +1,6 @@
+import networkx as nx
 import numpy as np
+import tqdm
 from numba import njit
 
 from .common import similarity
@@ -19,3 +21,19 @@ def adjacency_matrix(repeats, threshold):
     sm = sm >= threshold
     sm[np.diag_indices_from(sm)] = False
     return sm
+
+
+@njit
+def _edges_for_repeat(repeats, repeat, threshold):
+    out = np.zeros(len(repeats), dtype=np.bool_)
+    for i, r in enumerate(repeats):
+        out[i] = similarity(repeat, r) >= threshold
+    return out
+
+
+def build_graph(repeats, threshold, progress=False):
+    graph = nx.Graph()
+    for i, repeat in enumerate(tqdm.tqdm(repeats, disable=not progress)):
+        edge = _edges_for_repeat(repeats[i:], repeat, threshold)
+        graph.add_edges_from([(i, j) for j in np.nonzero(edge)[0]])
+    return graph
