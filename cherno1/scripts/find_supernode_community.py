8f24275be98adc3846d0a65f56a6df9167abf9ae
Mauro Silberberg <maurosilber@gmail.com>
2020-07-23 22:29:27 -0300

    Separate find supernode comunities

--- cherno1/scripts/find_supernode_community.py
+++ cherno1/scripts/find_supernode_community.py
@@ -1,32 +1,22 @@
-from functools import partial
+from pathlib import Path
 
 import click
 import numpy as np
-import tqdm
 from cherno1.common import castdown
 from cherno1.graph import build_igraph
 from p_tqdm import p_umap
 
 
-def func_joined(sn_repeats, sn, min_similarity, progress=False):
-    degree = np.zeros((len(sn_repeats), 34), dtype=int)
-    partitions = np.zeros((len(sn_repeats), 34), dtype=int)
-    for s in tqdm.trange(min_similarity, 33, disable=not progress):
-        graph = build_igraph(sn_repeats, s)
-        degree[:, s] = graph.degree()
-        partitions[:, s] = graph.community_infomap().membership
+def func(supernode_repeats, supernode, similarity):
+    p = Path(f"degree_{supernode}_{similarity}.npy")
+    if p.exists():
+        return
 
-    np.save(f"degree_{sn}.npy", castdown(degree))
-    np.save(f"partitions_{sn}.npy", castdown(partitions))
-
-
-def func_separated(sn_repeats, sn, min_similarity, progress=False):
-    for s in tqdm.trange(33, min_similarity, -1, disable=not progress):
-        graph = build_igraph(sn_repeats, s)
-        degree = graph.degree()
-        partitions = graph.community_infomap().membership
-        np.save(f"degree_{sn}_{s}.npy", castdown(degree))
-        np.save(f"partitions_{sn}_{s}.npy", castdown(partitions))
+    graph = build_igraph(supernode_repeats, similarity)
+    degree = graph.degree()
+    partitions = graph.community_infomap().membership
+    np.save(p, castdown(degree))
+    np.save(f"partitions_{supernode}_{similarity}.npy", castdown(partitions))
 
 
 @click.command()
@@ -36,15 +26,23 @@ def func_separated(sn_repeats, sn, min_similarity, progress=False):
 @click.option(
     "--min-similarity", type=click.INT, default=10, help="Upto this similarity.",
 )
-@click.option("--separated", is_flag=True)
-def cli(repeat_file, supernode_file, supernodes, min_similarity, separated):
+def cli(repeat_file, supernode_file, supernodes, min_similarity):
     repeats = np.load(repeat_file)["repeats"]
     supernode_array = np.load(supernode_file)["25"]
+
     if not supernodes:
         supernodes = np.unique(supernode_array)
-    func = func_separated if separated else func_joined
-    supernode_repeats = (repeats[supernode_array == sn] for sn in supernodes)
-    p_umap(partial(func, min_similarity=min_similarity), supernode_repeats, supernodes)
+
+    def _data(supernodes, min_similarity):
+        for sn in supernodes:
+            for s in range(33, min_similarity, -1):
+                yield repeats[supernode_array == sn], sn, s
+
+    p_umap(
+        lambda x, y: func(*x),
+        _data(supernodes, min_similarity),
+        range(len(supernodes) * (33 - min_similarity)),
+    )
 
 
 if __name__ == "__main__":
